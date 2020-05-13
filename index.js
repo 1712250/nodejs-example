1b9f2b3a0cf3add9f2e8d3a491a550eff66086f3
Hoang-Vu Le <lhvubtqn@gmail.com>
2020-05-13 09:03:31 +0700

    edit port

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnif
 const home = require("./routes/")
 app.use("/", home);
 
-app.listen(3000, () => console.log("Listening on port 3000..."));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Listening on port " + port));
 
 
 
